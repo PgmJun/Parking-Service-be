bf5300390fe1edecb38f8528dce208622d926875
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-20 23:23:27 +0900

    주석 추가

---
+++ src/main/java/jh/ParkingService/config/UserConfig.java
@@ -0,0 +1,44 @@
+package jh.ParkingService.config;
+
+import jh.ParkingService.repository.park.JpaParkRepository;
+import jh.ParkingService.repository.park.ParkRepository;
+import jh.ParkingService.repository.user.JpaUserRepository;
+import jh.ParkingService.repository.user.UserRepository;
+import jh.ParkingService.service.park.ParkServiceImpl;
+import jh.ParkingService.service.user.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class UserConfig {
+    private DataSource dataSource;
+    private EntityManager em;
+
+    @Autowired
+    public UserConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+
+
+    @Bean
+    public ParkRepository parkingRepository() { return new JpaParkRepository(em);}
+
+    @Bean
+    public ParkServiceImpl parkingService() { return new ParkServiceImpl(parkingRepository()); }
+
+    @Bean
+    public UserRepository userRepository() {
+        return new JpaUserRepository(em);
+    }
+
+    @Bean
+    public UserServiceImpl userService(){
+        return new UserServiceImpl(userRepository());
+    }
+
+}
